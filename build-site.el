0b8537763ad4af928265c38cac19d3fe5f865fd3
Luis Victoria <v@lambda.lv>
2025-02-20 01:53:54 -0500

    Improve the page styling

--- build-site.el
+++ build-site.el
@@ -1,6 +1,12 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
+;; Customize the HTML output
+(setq org-html-vlidation-link nil              ;; Don't show validation link
+      org-html-head-include-scripts nil        ;; Use our own scripts
+      org-html-head-include-default-style nil  ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
