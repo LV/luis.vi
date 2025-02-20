3469be4b9865e0453fc73eb0955415385eaad673
Luis Victoria <v@lambda.lv>
2025-02-20 01:50:51 -0500

    Set some site meta variables

--- build-site.el
+++ build-site.el
@@ -8,7 +8,15 @@
 	     :recursive t
 	     :base-directory "./content"
 	     :publishing-directory "./public"
-	     :publishing-function 'org-html-publish-to-html)))
+	     :publishing-function 'org-html-publish-to-html
+	     :author "Luis Victoria"
+	     :email "v@lambda.lv"
+	     :with-author nil      ;; Don't include author name
+	     :with-creator nil     ;; Don't include Emacs and Org versions in footer
+	     :with-toc nil         ;; Don't include a table of contents
+	     :section-numbers nil  ;; Don't include section numbers
+	     :time-stamp-file nil  ;; Don't include time stamp in file
+	     )))
 
 ;; Generate the site output
 (org-publish-all t)
