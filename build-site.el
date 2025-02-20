9f9562a05f4d1abb3768156d10f34c963d118f5f
Luis Victoria <v@lambda.lv>
2025-02-20 01:35:01 -0500

    Set up the publish project

--- build-site.el
+++ build-site.el
@@ -1,4 +1,16 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "luis.vi"
+	     :recursive t
+	     :base-directory "./content"
+	     :publishing-directory "./public"
+	     :publishing-function 'org-html-publish-to-html)))
+
+;; Generate the site output
+(org-publish-all t)
+
 (message "Build complete!")
