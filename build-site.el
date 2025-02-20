e41a535a320a2660406b81e34453fa3c725e42c3
Luis Victoria <v@lambda.lv>
2025-02-20 02:05:48 -0500

    Fix typo and remove validation link

--- build-site.el
+++ build-site.el
@@ -17,7 +17,7 @@
 (require 'ox-publish)
 
 ;; Customize the HTML output
-(setq org-html-vlidation-link nil              ;; Don't show validation link
+(setq org-html-validation-link nil             ;; Don't show validation link
       org-html-head-include-scripts nil        ;; Use our own scripts
       org-html-head-include-default-style nil  ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
